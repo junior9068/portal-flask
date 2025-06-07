40d93d2964fbdb4e5a4156d41d0b272b3484c9a5
Edilson Junior <edilsonjuniorti@gmail.com>
2025-06-06 23:56:53 -0300

    Adiciona arquivo de log

---
+++ funcoes/log.py
@@ -0,0 +1,12 @@
+import logging
+
+def teste1():
+# Configuração para os Logs da aplicação
+    return logging.basicConfig(
+        level=logging.INFO,
+        format='%(asctime)s [%(levelname)s] %(message)s',
+        handlers=[
+            logging.FileHandler("app.log"),  # salva em arquivo
+            logging.StreamHandler()         # mostra no terminal
+        ]
+    )
